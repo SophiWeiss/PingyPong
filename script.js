d63f0c55bfca98a79edc27902c620c4744c1555c
Sophi Weiss <Owlety34@gmail.com>
2023-01-16 23:16:00 +0100

    some improvements

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.getElementById("preferences-button").addEventListener("click", function() {
+//   let preferencesMenu = document.getElementById("preferences-menu");
+//   if (preferencesMenu.style.display === "none") {
+//     preferencesMenu.style.display = "block";
+//   } else {
+//     preferencesMenu.style.display = "none";
+//   }
+// });
+
+
+document.getElementById("preferences-button").addEventListener("click", function() {
+  let preferencesMenu = document.getElementById("preferences-menu");
+  if (preferencesMenu.style.display === "none") {
+    preferencesMenu.style.display = "block";
+  } else {
+    preferencesMenu.style.display = "none";
+  }
+});
